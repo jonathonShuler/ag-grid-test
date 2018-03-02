6dfea44668296fe70704d8fac85c195ebe393320
Jonathon Shuler <jonathon.shuler@gmail.com>
2018-03-02 10:49:36 -0700

    Confirm app is working

--- js/MainController.js
+++ js/MainController.js
@@ -1,8 +1,7 @@
-function MainController() {
-
+function MainController($scope) {
+  $scope.message = 'It Is Working!';
 }
 
 angular
   .module('ag-app')
   .controller('MainController', MainController);
-  
